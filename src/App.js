e3b8bb32af2cf8ff529dc67c81fd3691e3525b9f
i.surganov@itransition.com <i.surganov@itransition.com>
2023-05-23 19:46:18 +0400

    Section 5: Understanding 'keys'.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 import './App.css';
 
+const INITIAL_EXPENCES = [
+  { id: 1, title: 'Toilet paper', amount: 294.67, date: new Date(2021, 2, 28) },
+  { id: 2, title: 'Car insurance', amount: 394.67, date: new Date(2022, 2, 29) },
+  { id: 3, title: 'New desk (wooden)', amount: 394.67, date: new Date(2023, 2, 30) },
+];
+
 const App = () => {
-  const expenses = [
-    { id: 1, title: 'Toilet paper', amount: 294.67, date: new Date(2021, 2, 28) },
-    { id: 2, title: 'Car insurance', amount: 394.67, date: new Date(2021, 2, 29) },
-    { id: 3, title: 'New desk (wooden)', amount: 394.67, date: new Date(2021, 2, 30) },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENCES);
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((previousExpences => {
+      return [expense, ...previousExpences];
+    }));
   };
 
   return (
